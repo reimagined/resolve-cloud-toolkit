f1694e6ee6e44aeca307d2998ffcfc2feaf978e7
max <mozg.mayhem@gmail.com>
2020-06-05 13:32:10 +0300

    observer

---
+++ observer/archive.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+const fs = require('fs')
+const path = require('path')
+const { execSync } = require('child_process')
+const archiver = require('archiver')
+
+const zipAsset = async (directory, result) => {
+  const resultOutput = path.resolve(__dirname, result)
+  try {
+    await execSync(`zip -r -9 --quiet ${JSON.stringify(resultOutput)} .`, {
+      cwd: directory
+    })
+  } catch (error) {
+    console.warn(error.message)
+    await new Promise((resolve, reject) => {
+      const options = {
+        zlib: {
+          level: 9
+        }
+      }
+
+      const output = fs.createWriteStream(resultOutput)
+
+      const archive = archiver('zip', options)
+
+      archive.directory(directory, false)
+
+      archive.pipe(output)
+      archive.on('finish', resolve)
+      archive.on('error', reject)
+
+      archive.finalize()
+    })
+  }
+}
+
+zipAsset(process.argv[2], process.argv[3])
